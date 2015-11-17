ab977f3db1b054dfe17abfb9a4d277313a43b100
Anders Bergh <anders1@gmail.com>
2015-11-17 21:46:59 +0100

    fix cloud track error

--- iTunes/refresh_selection.js
+++ iTunes/refresh_selection.js
@@ -7,6 +7,8 @@ Progress.totalUnitCount = selection.length;
 
 var i = 0;
 for (var sel in selection) {
-    selection[sel].refresh();
+	try {
+	    selection[sel].refresh();
+	} catch (e) { } // cloud tracks don't understand refresh(), and I don't know how to tell if a track is local or not
     Progress.completedUnitCount = ++i;
 }
